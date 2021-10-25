4fdc98bccbaa032ad0fba44fc0d3168915c4e762
Tom <thomas.deal@gmail.com>
2021-10-25 10:46:33 -0400

    Remove files not required by sub branch.

--- test/SampleElement.m
+++
@@ -1,6 +0,0 @@
-%% Element Design
-Element.type = 4;                   % Rectangular piston
-Element.params_m = [lambda/2; ...   % Element face width, m
-                    lambda/4; ...   % Element face height, m
-                    0];
-Element.baffle = 1;                 % Hard Baffle
